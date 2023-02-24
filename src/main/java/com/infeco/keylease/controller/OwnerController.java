59891e4774ea213d77686f89328d2a50d6ab7dcf
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2023-02-24 14:03:42 +0100

    Added OwnerController GET /tenants

---
+++ src/main/java/com/infeco/keylease/controller/OwnerController.java
@@ -0,0 +1,25 @@
+package com.infeco.keylease.controller;
+
+import com.infeco.keylease.models.Owner;
+import com.infeco.keylease.security.AuthoritiesConstants;
+import com.infeco.keylease.service.OwnerService;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @GetMapping("/owners")
+    @PreAuthorize("hasAuthority('" + AuthoritiesConstants.USER + "')")
+    public List<Owner> getOwners(){
+        return this.ownerService.getOwners();
+    }
+}
