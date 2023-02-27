13ace77b99e25915aafa37e0395b2340ad428167
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2023-02-27 10:48:03 +0100

    POST /owner -> TDD - first step :  invalid code expected

--- src/main/java/com/infeco/keylease/service/OwnerService.java
+++ src/main/java/com/infeco/keylease/service/OwnerService.java
@@ -1,5 +1,8 @@
 package com.infeco.keylease.service;
 
+import com.infeco.keylease.entity.AddressEntity;
+import com.infeco.keylease.entity.OwnerEntity;
+import com.infeco.keylease.models.Address;
 import com.infeco.keylease.models.Owner;
 import com.infeco.keylease.repository.OwnerRepository;
 import org.springframework.stereotype.Service;
@@ -18,5 +21,9 @@ public class OwnerService {
 
     public List<Owner> getOwners() {
         return this.ownerRepository.findAll().stream().map(Owner::new).collect(Collectors.toList());
+    }
+
+    public Owner addOwner(Owner owner) {
+        return new Owner();
     }
 }
--- src/test/java/com/infeco/keylease/OwnerServiceTest.java
+++ src/test/java/com/infeco/keylease/OwnerServiceTest.java
@@ -11,12 +11,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
@@ -58,5 +62,57 @@ public class OwnerServiceTest {
         assert (expectedOwner.get(0).getFirstName()).equals(owners.get(0).getFirstName());
         assert (expectedOwner.get(0).getAddress().getStreet()).equals(owners.get(0).getAddress().getStreet());
         verify(ownerRepository).findAll();
+    }
+
+    @Test
+    public void testAddOwner(){
+        // Given
+        Owner owner = new Owner();
+        owner.setFirstName("FirstName");
+        owner.setLastName("LastName");
+        owner.setEmail("email@example.com");
+        owner.setPhoneNumber("0987654321");
+        owner.setRib("FR76 0000 0000 0000 0000 0000 000");
+
+        OwnerEntity ownerEntity = new OwnerEntity();
+        ownerEntity.setFirstName("FirstName");
+        ownerEntity.setLastName("LastName");
+        ownerEntity.setEmail("email@example.com");
+        ownerEntity.setPhoneNumber("0987654321");
+        ownerEntity.setRib("FR76 0000 0000 0000 0000 0000 000");
+
+        Address address = new Address();
+        owner.setAddress(address);
+        address.setStreet("1 rue des Lilas");
+        address.setAdditionalAddress("Bat A1 apt 11");
+        address.setZipCode("33130");
+        address.setTown("Bègles");
+
+        AddressEntity addressEntity = new AddressEntity();
+        addressEntity.setStreet("1 rue des Lilas");
+        addressEntity.setAdditionalAddress("Bat A1 apt 11");
+        addressEntity.setZipCode("33130");
+        addressEntity.setTown("Bègles");
+        ownerEntity.setAddress(addressEntity);
+
+        Mockito.when(ownerRepository.save(any(OwnerEntity.class))).thenReturn(ownerEntity);
+        Mockito.when(addressRepository.save(any(AddressEntity.class))).thenReturn(addressEntity);
+
+        // When
+        Owner savedOwner = ownerService.addOwner(owner);
+
+        // Then
+        assertNotNull(savedOwner);
+        assertEquals("FirstName",savedOwner.getFirstName());
+        assertEquals("LastName",savedOwner.getLastName());
+        assertEquals("email@example.com",savedOwner.getEmail());
+        assertEquals("0987654321",savedOwner.getPhoneNumber());
+        assertEquals("FR76 0000 0000 0000 0000 0000 000",savedOwner.getRib());
+
+        assertNotNull(savedOwner.getAddress());
+        assertEquals("1 rue des Lilas",savedOwner.getAddress().getStreet());
+        assertEquals("Bat A1 apt 11",savedOwner.getAddress().getAdditionalAddress());
+        assertEquals("33130",savedOwner.getAddress().getZipCode());
+        assertEquals("Bègles",savedOwner.getAddress().getTown());
     }
 }
