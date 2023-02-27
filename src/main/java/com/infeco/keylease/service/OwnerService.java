f616cecca743208e80eca16e7c1822bf938064e4
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2023-02-27 11:08:14 +0100

    POST /owner -> TDD - second step :  write method corresponding to the test -> test passed

--- src/main/java/com/infeco/keylease/service/OwnerService.java
+++ src/main/java/com/infeco/keylease/service/OwnerService.java
@@ -24,6 +24,36 @@ public class OwnerService {
     }
 
     public Owner addOwner(Owner owner) {
-        return new Owner();
+            OwnerEntity ownerEntity = new OwnerEntity();
+            ownerEntity.setFirstName(owner.getFirstName());
+            ownerEntity.setLastName(owner.getLastName());
+            ownerEntity.setEmail(owner.getEmail());
+            ownerEntity.setPhoneNumber(owner.getPhoneNumber());
+            ownerEntity.setRib(owner.getRib());
+
+            AddressEntity addressEntity = new AddressEntity();
+            addressEntity.setStreet(owner.getAddress().getStreet());
+            addressEntity.setAdditionalAddress(owner.getAddress().getAdditionalAddress());
+            addressEntity.setZipCode(owner.getAddress().getZipCode());
+            addressEntity.setTown(owner.getAddress().getTown());
+            ownerEntity.setAddress(addressEntity);
+
+            OwnerEntity savedOwnerEntity = this.ownerRepository.save(ownerEntity);
+
+            Owner savedOwner = new Owner();
+            savedOwner.setFirstName(savedOwnerEntity.getFirstName());
+            savedOwner.setLastName(savedOwnerEntity.getLastName());
+            savedOwner.setEmail(savedOwnerEntity.getEmail());
+            savedOwner.setPhoneNumber(savedOwnerEntity.getPhoneNumber());
+            savedOwner.setRib(savedOwnerEntity.getRib());
+
+            Address savedAddress = new Address();
+            savedAddress.setStreet(savedOwnerEntity.getAddress().getStreet());
+            savedAddress.setAdditionalAddress(savedOwnerEntity.getAddress().getAdditionalAddress());
+            savedAddress.setZipCode(savedOwnerEntity.getAddress().getZipCode());
+            savedAddress.setTown(savedOwnerEntity.getAddress().getTown());
+            savedOwner.setAddress(savedAddress);
+
+            return savedOwner;
     }
 }
