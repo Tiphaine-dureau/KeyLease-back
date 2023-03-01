55e8a80e73a2838c9b008e41a54eb8a35cbbac84
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2023-03-01 17:47:18 +0100

    Added missing unit tests for tenant controller and service

--- src/test/java/com/infeco/keylease/TenantControllerTest.java
+++ src/test/java/com/infeco/keylease/TenantControllerTest.java
@@ -1,17 +1,30 @@
 package com.infeco.keylease;
 
+import com.infeco.keylease.models.Address;
+import com.infeco.keylease.models.Tenant;
 import com.infeco.keylease.security.AuthoritiesConstants;
+import com.infeco.keylease.service.TenantService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -21,11 +34,180 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class TenantControllerTest {
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private TenantService tenantService;
 
     @Test
     @WithMockUser(authorities = AuthoritiesConstants.USER)
     public void testGetTenantsWithUserRole() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/tenants"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testGetTenantsWithoutRole() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/tenants"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(authorities = AuthoritiesConstants.USER)
+    public void testAddTenant() throws Exception {
+        // Création d'un locataire factice
+        Tenant tenant = new Tenant();
+        tenant.setFirstName("John");
+        tenant.setLastName("Doe");
+        tenant.setPhoneNumber("0756432189");
+        tenant.setEmail("john@example.com");
+        tenant.setPartnerFirstName("Jane");
+        tenant.setPartnerLastName("Doe");
+        tenant.setPartnerPhoneNumber("0756432188");
+
+        Address address = new Address();
+        address.setStreet("1 rue des Pivoines");
+        address.setAdditionalAddress("B Apt 8");
+        address.setZipCode("33000");
+        address.setTown("Bordeaux");
+        tenant.setAddress(address);
+
+        // Mock du service pour renvoyer le locataire factice
+        when(tenantService.addTenant(any(Tenant.class))).thenReturn(tenant);
+        // Construction de la requête POST pour ajouter un locataire
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/tenants")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"firstName\": \"John\", \"lastName\": \"Doe\", \"phoneNumber\": \"0756432189\", " +
+                        "\"email\": \"john@example.com\", " +
+                        "\"address\": {\"street\": \"1 rue des Pivoines\", \"additionalAddress\": \"B Apt 8\", \"zipCode\": \"33000\", \"town\": \"Bordeaux\"}," +
+                        "\"partnerFirstName\": \"Jane\", \"partnerLastName\": \"Doe\", \"partnerPhoneNumber\": \"0756432188\"}");
+
+        // Exécution de la requête et vérification du résultat
+        MvcResult result = mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("{\"firstName\": \"John\", \"lastName\": \"Doe\", \"phoneNumber\": \"0756432189\", " +
+                        "\"email\": \"john@example.com\", " +
+                        "\"address\": {\"street\": \"1 rue des Pivoines\", \"additionalAddress\": \"B Apt 8\", \"zipCode\": \"33000\", \"town\": \"Bordeaux\"}," +
+                        "\"partnerFirstName\": \"Jane\", \"partnerLastName\": \"Doe\", \"partnerPhoneNumber\": \"0756432188\"}"))
+                .andReturn();
+    }
+
+    @Test
+    @WithMockUser(authorities = AuthoritiesConstants.USER)
+    public void testPutTenant() throws Exception {
+        // Création d'un locataire factice
+        Tenant tenant = new Tenant();
+        tenant.setId(UUID.randomUUID());
+        tenant.setFirstName("John");
+        tenant.setLastName("Doe");
+        tenant.setPhoneNumber("0756432189");
+        tenant.setEmail("john@example.com");
+        tenant.setPartnerFirstName("Jane");
+        tenant.setPartnerLastName("Doe");
+        tenant.setPartnerPhoneNumber("0756432188");
+
+        Address address = new Address();
+        address.setStreet("1 rue des Pivoines");
+        address.setAdditionalAddress("B Apt 8");
+        address.setZipCode("33000");
+        address.setTown("Bordeaux");
+        tenant.setAddress(address);
+
+        // Mock du service pour renvoyer le locataire factice
+        when(tenantService.modifyTenant(any(Tenant.class), any(UUID.class))).thenReturn(tenant);
+
+        // Construction de la requête POST pour ajouter un locataire
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .put("/tenants/{id}", tenant.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"id\": \"" + tenant.getId() + "\", \"firstName\": \"John\", \"lastName\": \"Doe\"," +
+                        " \"phoneNumber\": \"0756432189\", \"email\": \"john@example.com\", " +
+                        "\"address\": {\"street\": \"1 rue des Pivoines\", \"additionalAddress\": \"B Apt 8\", \"zipCode\": \"33000\", \"town\": \"Bordeaux\"}," +
+                        "\"partnerFirstName\": \"Jane\", \"partnerLastName\": \"Doe\", \"partnerPhoneNumber\": \"0756432188\"}");
+
+        // Exécution de la requête et vérification du résultat
+        MvcResult result = mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("{\"id\": \"" + tenant.getId() + "\", \"firstName\": \"John\", \"lastName\": \"Doe\", " +
+                        "\"phoneNumber\": \"0756432189\", \"email\": \"john@example.com\"," +
+                        " \"address\": {\"street\": \"1 rue des Pivoines\", \"additionalAddress\": \"B Apt 8\", \"zipCode\": \"33000\", \"town\": \"Bordeaux\"}," +
+                        "\"partnerFirstName\": \"Jane\", \"partnerLastName\": \"Doe\", \"partnerPhoneNumber\": \"0756432188\"}"))
+                .andReturn();
+
+    }
+
+    @Test
+    @WithMockUser(authorities = AuthoritiesConstants.USER)
+    public void testGetTenantById() throws Exception {
+        // Création d'un locataire factice
+        UUID tenantId = UUID.randomUUID();
+        Tenant tenant = new Tenant();
+        tenant.setId(tenantId);
+        tenant.setFirstName("John");
+        tenant.setLastName("Doe");
+        tenant.setPhoneNumber("0756432189");
+        tenant.setEmail("john@example.com");
+        tenant.setPartnerFirstName("Jane");
+        tenant.setPartnerLastName("Doe");
+        tenant.setPartnerPhoneNumber("0756432188");
+
+        Address address = new Address();
+        address.setStreet("1 rue des Pivoines");
+        address.setAdditionalAddress("B Apt 8");
+        address.setZipCode("33000");
+        address.setTown("Bordeaux");
+        tenant.setAddress(address);
+
+        // Mock du service pour renvoyer le locataire factice
+        when(tenantService.getTenantById(tenantId)).thenReturn(tenant);
+
+        // Construction de la requête GET pour récupérer un locataire
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/tenants/{id}", tenantId)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        // Exécution de la requête et vérification du résultat
+        MvcResult result = mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("{\"id\": \"" + tenant.getId() + "\", \"firstName\": \"John\", \"lastName\": \"Doe\", " +
+                        "\"phoneNumber\": \"0756432189\", \"email\": \"john@example.com\"," +
+                        " \"address\": {\"street\": \"1 rue des Pivoines\", \"additionalAddress\": \"B Apt 8\", \"zipCode\": \"33000\", \"town\": \"Bordeaux\"}," +
+                        "\"partnerFirstName\": \"Jane\", \"partnerLastName\": \"Doe\", \"partnerPhoneNumber\": \"0756432188\"}"))
+                .andReturn();
+    }
+
+    @Test
+    @WithMockUser(authorities = AuthoritiesConstants.USER)
+    public void testDeleteTenant() throws Exception {
+        // Création d'un locataire factice
+        UUID tenantId = UUID.randomUUID();
+        Tenant tenant = new Tenant();
+        tenant.setId(tenantId);
+        tenant.setFirstName("John");
+        tenant.setLastName("Doe");
+        tenant.setPhoneNumber("0756432189");
+        tenant.setEmail("john@example.com");
+        tenant.setPartnerFirstName("Jane");
+        tenant.setPartnerLastName("Doe");
+        tenant.setPartnerPhoneNumber("0756432188");
+
+        Address address = new Address();
+        address.setStreet("1 rue des Pivoines");
+        address.setAdditionalAddress("B Apt 8");
+        address.setZipCode("33000");
+        address.setTown("Bordeaux");
+        tenant.setAddress(address);
+
+        // Mock du service pour renvoyer le locataire factice
+        doNothing().when(tenantService).deleteTenant(tenant.getId());
+
+        // Construction de la requête DELETE pour supprimer un locataire
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .delete("/tenants/{id}", tenant.getId())
+                .contentType(MediaType.APPLICATION_JSON);
+
+        // Exécution de la requête et vérification du résultat
+        MvcResult result = mockMvc.perform(requestBuilder)
+                .andExpect(status().isNoContent())
+                .andReturn();
     }
 }
--- src/test/java/com/infeco/keylease/TenantServiceTest.java
+++ src/test/java/com/infeco/keylease/TenantServiceTest.java
@@ -2,6 +2,7 @@ package com.infeco.keylease;
 
 import com.infeco.keylease.entity.AddressEntity;
 import com.infeco.keylease.entity.TenantEntity;
+import com.infeco.keylease.exceptions.NotFoundEntity;
 import com.infeco.keylease.models.Address;
 import com.infeco.keylease.models.Tenant;
 import com.infeco.keylease.repository.AddressRepository;
@@ -17,12 +18,13 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -81,7 +83,6 @@ public class TenantServiceTest {
         tenantEntity.setPartnerLastName("Doe");
         tenantEntity.setPartnerPhoneNumber("1234567890");
 
-
         Address address = new Address();
         tenant.setAddress(address);
         address.setStreet("1 rue des mimosas");
@@ -101,6 +102,7 @@ public class TenantServiceTest {
 
         // When
         Tenant savedTenant = tenantService.addTenant(tenant);
+
         // Then
         assertNotNull(savedTenant);
         assertEquals("John", savedTenant.getFirstName());
@@ -116,6 +118,178 @@ public class TenantServiceTest {
         assertEquals("Bâtiment 1 apt 22", savedTenant.getAddress().getAdditionalAddress());
         assertEquals("33000", savedTenant.getAddress().getZipCode());
         assertEquals("Bordeaux", savedTenant.getAddress().getTown());
-
     }
+
+    @Test
+    public void testGetTenantById() throws NotFoundEntity {
+        UUID id = UUID.randomUUID();
+        Tenant tenant = new Tenant();
+        tenant.setId(id);
+        tenant.setFirstName("John");
+        tenant.setLastName("Doe");
+        tenant.setEmail("doe@example.com");
+        tenant.setPhoneNumber("0787654321");
+        tenant.setPartnerFirstName("Jane");
+        tenant.setPartnerLastName("Doe");
+        tenant.setPartnerPhoneNumber("0787654322");
+
+        TenantEntity tenantEntity = new TenantEntity();
+        tenantEntity.setFirstName("John");
+        tenantEntity.setLastName("Doe");
+        tenantEntity.setEmail("doe@example.com");
+        tenantEntity.setPhoneNumber("0787654321");
+        tenantEntity.setPartnerFirstName("Jane");
+        tenantEntity.setPartnerLastName("Doe");
+        tenantEntity.setPartnerPhoneNumber("0787654322");
+
+        Address address = new Address();
+        tenant.setAddress(address);
+        address.setStreet("1 rue des Lilas");
+        address.setAdditionalAddress("Bat C1 apt 22");
+        address.setZipCode("33130");
+        address.setTown("Bègles");
+
+        AddressEntity addressEntity = new AddressEntity();
+        addressEntity.setStreet("1 rue des Lilas");
+        addressEntity.setAdditionalAddress("Bat C1 apt 22");
+        addressEntity.setZipCode("33130");
+        addressEntity.setTown("Bègles");
+        tenantEntity.setAddress(addressEntity);
+
+        // Mock du repository pour renvoyer le propriétaire fictif
+        when(tenantRepository.findById(id)).thenReturn(Optional.of(tenantEntity));
+        // Exécution de la méthode à tester
+        Tenant tenantById = tenantService.getTenantById(id);
+
+        // Vérification du résultat
+        assertNotNull(tenantById);
+        assertEquals(id, tenant.getId());
+        assertEquals("John", tenant.getFirstName());
+        assertEquals("Doe", tenant.getLastName());
+        assertEquals("0787654321", tenant.getPhoneNumber());
+        assertEquals("doe@example.com", tenant.getEmail());
+        assertEquals("Jane", tenant.getPartnerFirstName());
+        assertEquals("Doe", tenant.getPartnerLastName());
+        assertNotNull(tenant.getAddress());
+        assertEquals("1 rue des Lilas", tenant.getAddress().getStreet());
+        assertEquals("Bat C1 apt 22", tenant.getAddress().getAdditionalAddress());
+        assertEquals("33130", tenant.getAddress().getZipCode());
+        assertEquals("Bègles", tenant.getAddress().getTown());
+    }
+
+    @Test
+    public void testPutTenant() throws NotFoundEntity {
+        // Création d'un locataire factice
+        Tenant tenant = new Tenant();
+        tenant.setFirstName("John");
+        tenant.setLastName("Doe");
+        tenant.setEmail("doe@example.com");
+        tenant.setPhoneNumber("0787654321");
+        tenant.setPartnerFirstName("Jane");
+        tenant.setPartnerLastName("Doe");
+        tenant.setPartnerPhoneNumber("0787654322");
+
+        TenantEntity tenantEntity = new TenantEntity();
+        tenantEntity.setFirstName("John");
+        tenantEntity.setLastName("Doe");
+        tenantEntity.setEmail("doe@example.com");
+        tenantEntity.setPhoneNumber("0787654321");
+        tenantEntity.setPartnerFirstName("Jane");
+        tenantEntity.setPartnerLastName("Doe");
+        tenantEntity.setPartnerPhoneNumber("0787654322");
+
+        Address address = new Address();
+        tenant.setAddress(address);
+        address.setStreet("1 rue des Lilas");
+        address.setAdditionalAddress("Bat C1 apt 22");
+        address.setZipCode("33130");
+        address.setTown("Bègles");
+
+        AddressEntity addressEntity = new AddressEntity();
+        addressEntity.setStreet("1 rue des Lilas");
+        addressEntity.setAdditionalAddress("Bat C1 apt 22");
+        addressEntity.setZipCode("33130");
+        addressEntity.setTown("Bègles");
+        tenantEntity.setAddress(addressEntity);
+
+        // Création d'un TenantEntity existant
+        TenantEntity existingTenantEntity = new TenantEntity();
+        existingTenantEntity.setId(UUID.randomUUID());
+        existingTenantEntity.setPartnerFirstName("ExistingTenant");
+        existingTenantEntity.setAddress(new AddressEntity());
+
+        // Simulation de la recherche du TenantEntity modifié
+        when(tenantRepository.findById(existingTenantEntity.getId())).thenReturn(Optional.of(existingTenantEntity));
+
+        // Simulation de la sauvegarde du TenantEntity modifié
+        when(tenantRepository.save(any(TenantEntity.class))).thenAnswer(invocationOnMock -> {
+            TenantEntity savedTenantEntity = invocationOnMock.getArgument(0);
+            savedTenantEntity.setId(existingTenantEntity.getId());
+            return savedTenantEntity;
+        });
+
+        // Appel de la méthode à tester pour modifier un Tenant existant
+        Tenant modifiedTenant = tenantService.modifyTenant(tenant, existingTenantEntity.getId());
+
+        // Vérification que TenantEntity a été modifié correctement
+        assertEquals(tenant.getFirstName(), modifiedTenant.getFirstName());
+        assertEquals(tenant.getAddress().getStreet(), modifiedTenant.getAddress().getStreet());
+        assertEquals(tenant.getAddress().getAdditionalAddress(), modifiedTenant.getAddress().getAdditionalAddress());
+        assertEquals(tenant.getAddress().getZipCode(), modifiedTenant.getAddress().getZipCode());
+        assertEquals(tenant.getAddress().getTown(), modifiedTenant.getAddress().getTown());
+        // Vérification que la méthode modifyTenant lève bien une exception NotFoundEntity
+        assertThrows(NotFoundEntity.class, () -> tenantService.modifyTenant(tenant, UUID.randomUUID()));
+        // Vérification que la méthode findById est appelée avec l'id de existingTenantEntity
+        verify(tenantRepository).findById(existingTenantEntity.getId());
+        // Vérification que la méthode save est appelée avec le TenantEntity modifié
+        verify(tenantRepository).save(any(TenantEntity.class));
+    }
+
+    @Test
+    public void testDeleteTenant() throws Exception {
+        // Création d'un locataire factice
+        UUID id = UUID.randomUUID();
+        Tenant tenant = new Tenant();
+        tenant.setId(id);
+        tenant.setFirstName("John");
+        tenant.setLastName("Doe");
+        tenant.setEmail("doe@example.com");
+        tenant.setPhoneNumber("0787654321");
+        tenant.setPartnerFirstName("Jane");
+        tenant.setPartnerLastName("Doe");
+        tenant.setPartnerPhoneNumber("0787654322");
+
+        TenantEntity tenantEntity = new TenantEntity();
+        tenantEntity.setFirstName("John");
+        tenantEntity.setLastName("Doe");
+        tenantEntity.setEmail("doe@example.com");
+        tenantEntity.setPhoneNumber("0787654321");
+        tenantEntity.setPartnerFirstName("Jane");
+        tenantEntity.setPartnerLastName("Doe");
+        tenantEntity.setPartnerPhoneNumber("0787654322");
+
+        Address address = new Address();
+        tenant.setAddress(address);
+        address.setStreet("1 rue des Lilas");
+        address.setAdditionalAddress("Bat C1 apt 22");
+        address.setZipCode("33130");
+        address.setTown("Bègles");
+
+        AddressEntity addressEntity = new AddressEntity();
+        addressEntity.setStreet("1 rue des Lilas");
+        addressEntity.setAdditionalAddress("Bat C1 apt 22");
+        addressEntity.setZipCode("33130");
+        addressEntity.setTown("Bègles");
+        tenantEntity.setAddress(addressEntity);
+
+        // Mock du repository pour renvoyer le locataire factice
+        when(tenantRepository.findById(id)).thenReturn(Optional.of(tenantEntity));
+
+        // Appel de la méthode à tester
+        tenantService.deleteTenant(id);
+
+        // Vérification que la méthode delete du repository a bien été appelée avec le tenantEntity
+        verify(tenantRepository, times(1)).delete(tenantEntity);
+    }
+
 }
