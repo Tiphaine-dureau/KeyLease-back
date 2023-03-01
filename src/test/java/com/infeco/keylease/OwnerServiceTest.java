5c2eeeb2e02f9158e6801b83ca379ae9447f7b1a
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2023-03-01 14:15:27 +0100

    Added testGetOwnerById and testPutOwner

--- src/test/java/com/infeco/keylease/OwnerServiceTest.java
+++ src/test/java/com/infeco/keylease/OwnerServiceTest.java
@@ -2,6 +2,7 @@ package com.infeco.keylease;
 
 import com.infeco.keylease.entity.AddressEntity;
 import com.infeco.keylease.entity.OwnerEntity;
+import com.infeco.keylease.exceptions.NotFoundEntity;
 import com.infeco.keylease.models.Address;
 import com.infeco.keylease.models.Owner;
 import com.infeco.keylease.repository.AddressRepository;
@@ -17,24 +18,22 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @ActiveProfiles("dev")
 public class OwnerServiceTest {
-
     @Mock
     private OwnerRepository ownerRepository;
-
     @Mock
     private AddressRepository addressRepository;
-
     @InjectMocks
     OwnerService ownerService;
 
@@ -65,7 +64,7 @@ public class OwnerServiceTest {
     }
 
     @Test
-    public void testAddOwner(){
+    public void testAddOwner() {
         // Given
         Owner owner = new Owner();
         owner.setFirstName("FirstName");
@@ -103,16 +102,134 @@ public class OwnerServiceTest {
 
         // Then
         assertNotNull(savedOwner);
-        assertEquals("FirstName",savedOwner.getFirstName());
-        assertEquals("LastName",savedOwner.getLastName());
-        assertEquals("email@example.com",savedOwner.getEmail());
-        assertEquals("0987654321",savedOwner.getPhoneNumber());
-        assertEquals("FR76 0000 0000 0000 0000 0000 000",savedOwner.getIban());
+        assertEquals("FirstName", savedOwner.getFirstName());
+        assertEquals("LastName", savedOwner.getLastName());
+        assertEquals("email@example.com", savedOwner.getEmail());
+        assertEquals("0987654321", savedOwner.getPhoneNumber());
+        assertEquals("FR76 0000 0000 0000 0000 0000 000", savedOwner.getIban());
 
         assertNotNull(savedOwner.getAddress());
-        assertEquals("1 rue des Lilas",savedOwner.getAddress().getStreet());
-        assertEquals("Bat A1 apt 11",savedOwner.getAddress().getAdditionalAddress());
-        assertEquals("33130",savedOwner.getAddress().getZipCode());
-        assertEquals("Bègles",savedOwner.getAddress().getTown());
+        assertEquals("1 rue des Lilas", savedOwner.getAddress().getStreet());
+        assertEquals("Bat A1 apt 11", savedOwner.getAddress().getAdditionalAddress());
+        assertEquals("33130", savedOwner.getAddress().getZipCode());
+        assertEquals("Bègles", savedOwner.getAddress().getTown());
+    }
+
+    @Test
+    public void testGetOwnerById() throws NotFoundEntity {
+        // Création propriétaire à factice
+        UUID id = UUID.randomUUID();
+        Owner owner = new Owner();
+        owner.setId(id);
+        owner.setFirstName("John");
+        owner.setLastName("Doe");
+        owner.setEmail("doe@example.com");
+        owner.setPhoneNumber("0787654321");
+        owner.setIban("FR76 0001 0001 0001 0001 0001 001");
+
+        OwnerEntity ownerEntity = new OwnerEntity();
+        ownerEntity.setFirstName("John");
+        ownerEntity.setLastName("Doe");
+        ownerEntity.setEmail("doe@example.com");
+        ownerEntity.setPhoneNumber("0787654321");
+        ownerEntity.setIban("FR76 0001 0001 0001 0001 0001 001");
+
+        Address address = new Address();
+        owner.setAddress(address);
+        address.setStreet("1 rue des Lilas");
+        address.setAdditionalAddress("Bat C1 apt 22");
+        address.setZipCode("33130");
+        address.setTown("Bègles");
+
+        AddressEntity addressEntity = new AddressEntity();
+        addressEntity.setStreet("1 rue des Lilas");
+        addressEntity.setAdditionalAddress("Bat C1 apt 22");
+        addressEntity.setZipCode("33130");
+        addressEntity.setTown("Bègles");
+        ownerEntity.setAddress(addressEntity);
+
+        // Mock du repository pour renvoyer le propriétaire factice
+        when(ownerRepository.findById(id)).thenReturn(Optional.of(ownerEntity));
+        // Exécution de la méthode à tester
+        Owner ownerById = ownerService.getOwnerById(id);
+
+        // Vérification du résultat
+        assertNotNull(ownerById);
+        assertEquals(id, owner.getId());
+        assertEquals("John", owner.getFirstName());
+        assertEquals("Doe", owner.getLastName());
+        assertEquals("0787654321", owner.getPhoneNumber());
+        assertEquals("doe@example.com", owner.getEmail());
+        assertEquals("FR76 0001 0001 0001 0001 0001 001", owner.getIban());
+        assertNotNull(owner.getAddress());
+        assertEquals("1 rue des Lilas", owner.getAddress().getStreet());
+        assertEquals("Bat C1 apt 22", owner.getAddress().getAdditionalAddress());
+        assertEquals("33130", owner.getAddress().getZipCode());
+        assertEquals("Bègles", owner.getAddress().getTown());
+
+    }
+
+    @Test
+    public void testPutOwner() throws NotFoundEntity {
+        // Création propriétaire à factice
+        Owner owner = new Owner();
+        owner.setFirstName("John");
+        owner.setLastName("Doe");
+        owner.setEmail("doe@example.com");
+        owner.setPhoneNumber("0787654321");
+        owner.setIban("FR76 0001 0001 0001 0001 0001 001");
+
+        OwnerEntity ownerEntity = new OwnerEntity();
+        ownerEntity.setFirstName("John");
+        ownerEntity.setLastName("Doe");
+        ownerEntity.setEmail("doe@example.com");
+        ownerEntity.setPhoneNumber("0787654321");
+        ownerEntity.setIban("FR76 0001 0001 0001 0001 0001 001");
+
+        Address address = new Address();
+        owner.setAddress(address);
+        address.setStreet("1 rue des Lilas");
+        address.setAdditionalAddress("Bat C1 apt 22");
+        address.setZipCode("33130");
+        address.setTown("Bègles");
+
+        AddressEntity addressEntity = new AddressEntity();
+        addressEntity.setStreet("1 rue des Lilas");
+        addressEntity.setAdditionalAddress("Bat C1 apt 22");
+        addressEntity.setZipCode("33130");
+        addressEntity.setTown("Bègles");
+        ownerEntity.setAddress(addressEntity);
+
+        // Création d'un OwnerEntity existant
+        OwnerEntity existingOwnerEntity = new OwnerEntity();
+        existingOwnerEntity.setId(UUID.randomUUID());
+        existingOwnerEntity.setFirstName("ExistingOwner");
+        existingOwnerEntity.setAddress(new AddressEntity());
+
+        // Simulation de la recherche de l'OwnerEntity par l'id
+        when(ownerRepository.findById(existingOwnerEntity.getId())).thenReturn(Optional.of(existingOwnerEntity));
+
+        // Simulation de la sauvegarde de l'OwnerEntity modifié
+        when(ownerRepository.save(any(OwnerEntity.class))).thenAnswer(invocationOnMock -> {
+            OwnerEntity savedOwnerEntity = invocationOnMock.getArgument(0);
+            savedOwnerEntity.setId(existingOwnerEntity.getId());
+            return savedOwnerEntity;
+        });
+
+        //Appel de la méthode à tester pour modifier un Owner existant
+        Owner modifiedOwner = ownerService.modifyOwner(owner, existingOwnerEntity.getId());
+
+        // Vérification que l'OwnerEntity a été modifié correctement
+        assertEquals(owner.getFirstName(), modifiedOwner.getFirstName());
+        assertEquals(owner.getAddress().getStreet(), modifiedOwner.getAddress().getStreet());
+        assertEquals(owner.getAddress().getAdditionalAddress(), modifiedOwner.getAddress().getAdditionalAddress());
+        assertEquals(owner.getAddress().getZipCode(), modifiedOwner.getAddress().getZipCode());
+        assertEquals(owner.getAddress().getTown(), modifiedOwner.getAddress().getTown());
+        // Vérification que la méthode modifyOwner lève bien une exception NotFoundEntity
+        assertThrows(NotFoundEntity.class, () -> ownerService.modifyOwner(owner, UUID.randomUUID()));
+        // Vérification que la méthode findById est appelée avec l'id de existingOwnerEntity
+        verify(ownerRepository).findById(existingOwnerEntity.getId());
+        // Vérification que la méthode save est appelée avec l'OwnerEntity modifié
+        verify(ownerRepository).save(any(OwnerEntity.class));
     }
 }
