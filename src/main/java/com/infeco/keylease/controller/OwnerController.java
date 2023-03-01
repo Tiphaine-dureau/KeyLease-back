ba067a26cdece82ad98ec050806b0b95ad481d66
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2023-03-01 14:20:49 +0100

    Added methods getOwnerById and modifyOwner

--- src/main/java/com/infeco/keylease/controller/OwnerController.java
+++ src/main/java/com/infeco/keylease/controller/OwnerController.java
@@ -1,15 +1,16 @@
 package com.infeco.keylease.controller;
 
+import com.infeco.keylease.exceptions.NotFoundEntity;
 import com.infeco.keylease.models.Owner;
 import com.infeco.keylease.security.AuthoritiesConstants;
 import com.infeco.keylease.service.OwnerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class OwnerController {
@@ -21,13 +22,34 @@ public class OwnerController {
 
     @GetMapping("/owners")
     @PreAuthorize("hasAuthority('" + AuthoritiesConstants.USER + "')")
-    public List<Owner> getOwners(){
+    public List<Owner> getOwners() {
         return this.ownerService.getOwners();
+    }
+
+    @GetMapping("/owners/{id}")
+    @PreAuthorize("hasAuthority('" + AuthoritiesConstants.USER + "')")
+    public ResponseEntity<Owner> getOwnerById(@PathVariable UUID id) {
+        try {
+            return ResponseEntity.ok(this.ownerService.getOwnerById(id));
+        } catch (NotFoundEntity e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @PostMapping("/owners")
     @PreAuthorize("hasAuthority('" + AuthoritiesConstants.USER + "')")
-    public Owner addOwner(@RequestBody Owner owner){
+    public Owner addOwner(@RequestBody Owner owner) {
         return ownerService.addOwner(owner);
+    }
+
+    @PutMapping("/owners/{id}")
+    @PreAuthorize("hasAuthority('" + AuthoritiesConstants.USER + "')")
+    public ResponseEntity<Owner> modifyOwner(@RequestBody Owner owner, @PathVariable UUID id) {
+        try {
+            Owner modifiedOwner = ownerService.modifyOwner(owner, id);
+            return ResponseEntity.ok(modifiedOwner);
+        } catch (NotFoundEntity e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 }
