d1e9dcd2db45f1f05830459d64a221c7cd2ed317
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2023-03-10 14:04:25 +0100

    Added POST /properties on PropertyController

--- src/main/java/com/infeco/keylease/controller/PropertyController.java
+++ src/main/java/com/infeco/keylease/controller/PropertyController.java
@@ -1,10 +1,15 @@
 package com.infeco.keylease.controller;
 
+import com.infeco.keylease.exceptions.NotFoundEntity;
 import com.infeco.keylease.models.Property;
 import com.infeco.keylease.security.AuthoritiesConstants;
 import com.infeco.keylease.service.PropertyService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,5 +26,15 @@ public class PropertyController {
     @PreAuthorize("hasAuthority('" + AuthoritiesConstants.USER + "')")
     public List<Property> getProperties() {
         return this.propertyService.getProperties();
+    }
+
+    @PostMapping("/properties")
+    @PreAuthorize("hasAuthority('" + AuthoritiesConstants.USER + "')")
+    public ResponseEntity<Property> addProperty(@RequestBody Property property) {
+        try {
+            return ResponseEntity.ok(this.propertyService.addProperty(property));
+        } catch (NotFoundEntity e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 }
