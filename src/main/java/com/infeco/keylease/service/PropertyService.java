e1e3e4e5d56cefc21eff5acff3557f84514dbfe5
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2023-03-10 14:03:11 +0100

    Added POST /properties

--- src/main/java/com/infeco/keylease/service/PropertyService.java
+++ src/main/java/com/infeco/keylease/service/PropertyService.java
@@ -1,21 +1,87 @@
 package com.infeco.keylease.service;
 
+import com.infeco.keylease.entity.AddressEntity;
+import com.infeco.keylease.entity.PropertyEntity;
+import com.infeco.keylease.entity.PropertyTypeEntity;
+import com.infeco.keylease.exceptions.NotFoundEntity;
+import com.infeco.keylease.models.Address;
 import com.infeco.keylease.models.Property;
+import com.infeco.keylease.repository.AddressRepository;
 import com.infeco.keylease.repository.PropertyRepository;
+import com.infeco.keylease.repository.PropertyTypeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class PropertyService {
     private final PropertyRepository propertyRepository;
+    private final AddressRepository addressRepository;
 
-    public PropertyService(PropertyRepository propertyRepository) {
+    private final PropertyTypeRepository propertyTypeRepository;
+
+    public PropertyService(PropertyRepository propertyRepository, AddressRepository addressRepository, PropertyTypeRepository propertyTypeRepository) {
         this.propertyRepository = propertyRepository;
+        this.addressRepository = addressRepository;
+        this.propertyTypeRepository = propertyTypeRepository;
     }
 
     public List<Property> getProperties() {
         return this.propertyRepository.findAll().stream().map(Property::new).collect(Collectors.toList());
+    }
+
+    public Property addProperty(Property property) throws NotFoundEntity {
+        AddressEntity addressEntity = new AddressEntity();
+        addressToEntity(property.getAddress(), addressEntity);
+
+        PropertyEntity propertyEntity = new PropertyEntity();
+        propertyToEntity(property, propertyEntity);
+
+        PropertyTypeEntity propertyTypeEntity = new PropertyTypeEntity();
+        Optional<PropertyTypeEntity> propertyTypeEntityOptional = this.propertyTypeRepository.findById(property.getType());
+        if (propertyTypeEntityOptional.isPresent()) {
+            propertyTypeEntity = propertyTypeEntityOptional.get();
+        } else {
+            throw new NotFoundEntity();
+        }
+        propertyEntity.setPropertyType(propertyTypeEntity);
+
+        AddressEntity savedAddressEntity = this.addressRepository.save(addressEntity);
+        propertyEntity.setAddress(savedAddressEntity);
+        PropertyEntity savedPropertyEntity = this.propertyRepository.save(propertyEntity);
+        return entityToProperty(savedPropertyEntity);
+    }
+
+    private void propertyToEntity(Property property, PropertyEntity propertyEntity) {
+        propertyEntity.setArea(property.getArea());
+        propertyEntity.setRoomsNumber(property.getRoomsNumber());
+        propertyEntity.setDescription(property.getDescription());
+    }
+
+    private void addressToEntity(Address address, AddressEntity addressEntity) {
+        addressEntity.setStreet(address.getStreet());
+        addressEntity.setAdditionalAddress(address.getAdditionalAddress());
+        addressEntity.setZipCode(address.getZipCode());
+        addressEntity.setTown(address.getTown());
+    }
+
+    private Property entityToProperty(PropertyEntity propertyEntity) {
+        Property property = new Property();
+        Address address = new Address();
+        property.setArea(propertyEntity.getArea());
+        property.setRoomsNumber(propertyEntity.getRoomsNumber());
+        property.setDescription(propertyEntity.getDescription());
+
+        property.setType(propertyEntity.getPropertyType().getName());
+
+        address.setStreet(propertyEntity.getAddress().getStreet());
+        address.setAdditionalAddress(propertyEntity.getAddress().getAdditionalAddress());
+        address.setZipCode(propertyEntity.getAddress().getZipCode());
+        address.setTown(propertyEntity.getAddress().getTown());
+        property.setAddress(address);
+
+        return property;
     }
 }
