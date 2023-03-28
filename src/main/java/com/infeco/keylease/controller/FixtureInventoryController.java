86699cf4cf4202f423fc114aa0d9ec9a98599710
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2023-03-28 19:55:58 +0200

    Added POST /fixtures-inventory

--- src/main/java/com/infeco/keylease/controller/FixtureInventoryController.java
+++ src/main/java/com/infeco/keylease/controller/FixtureInventoryController.java
@@ -2,14 +2,13 @@ package com.infeco.keylease.controller;
 
 import com.infeco.keylease.exceptions.NotFoundEntity;
 import com.infeco.keylease.models.FixtureInventory;
+import com.infeco.keylease.models.PostFixtureInventory;
 import com.infeco.keylease.security.AuthoritiesConstants;
 import com.infeco.keylease.service.FixtureInventoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -33,6 +32,16 @@ public class FixtureInventoryController {
     public ResponseEntity<FixtureInventory> getFixtureInventoryById(@PathVariable UUID id) {
         try {
             return ResponseEntity.ok(this.fixtureInventoryService.getFixtureInventoryById(id));
+        } catch (NotFoundEntity e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/fixtures-inventory")
+    @PreAuthorize("hasAuthority('" + AuthoritiesConstants.USER + "')")
+    public ResponseEntity<FixtureInventory> addFixtureInventory(@RequestBody PostFixtureInventory fixtureInventory) {
+        try {
+            return ResponseEntity.ok(this.fixtureInventoryService.addFixtureInventory(fixtureInventory));
         } catch (NotFoundEntity e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
--- src/main/java/com/infeco/keylease/service/FixtureInventoryService.java
+++ src/main/java/com/infeco/keylease/service/FixtureInventoryService.java
@@ -1,9 +1,12 @@
 package com.infeco.keylease.service;
 
 import com.infeco.keylease.entity.FixtureInventoryEntity;
+import com.infeco.keylease.entity.PropertyEntity;
 import com.infeco.keylease.exceptions.NotFoundEntity;
 import com.infeco.keylease.models.FixtureInventory;
+import com.infeco.keylease.models.PostFixtureInventory;
 import com.infeco.keylease.repository.FixtureInventoryRepository;
+import com.infeco.keylease.repository.PropertyRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,9 +17,11 @@ import java.util.stream.Collectors;
 @Service
 public class FixtureInventoryService {
     private final FixtureInventoryRepository fixtureInventoryRepository;
+    private final PropertyRepository propertyRepository;
 
-    public FixtureInventoryService(FixtureInventoryRepository fixtureInventoryRepository) {
+    public FixtureInventoryService(FixtureInventoryRepository fixtureInventoryRepository, PropertyRepository propertyRepository) {
         this.fixtureInventoryRepository = fixtureInventoryRepository;
+        this.propertyRepository = propertyRepository;
     }
 
     public List<FixtureInventory> getFixturesInventory() {
@@ -30,6 +35,21 @@ public class FixtureInventoryService {
         } else {
             throw new NotFoundEntity();
         }
+    }
+
+    public FixtureInventory addFixtureInventory(PostFixtureInventory fixtureInventory) throws NotFoundEntity {
+        PropertyEntity propertyEntity = propertyRepository.findById(fixtureInventory.getPropertyId())
+                .orElseThrow(NotFoundEntity::new);
+
+        FixtureInventoryEntity fixtureInventoryEntity = new FixtureInventoryEntity();
+        fixtureInventoryEntity.setProperty(propertyEntity);
+        fixtureInventoryEntity.setArrivalFixtureInventoryDate(fixtureInventory.getArrivalFixtureInventoryDate());
+        fixtureInventoryEntity.setArrivalComments(fixtureInventory.getArrivalComments());
+        fixtureInventoryEntity.setExitFixtureInventoryDate(fixtureInventory.getExitFixtureInventoryDate());
+        fixtureInventoryEntity.setExitComments(fixtureInventory.getExitComments());
+
+        FixtureInventoryEntity savedFixtureInventoryEntity = fixtureInventoryRepository.save(fixtureInventoryEntity);
+        return new FixtureInventory(savedFixtureInventoryEntity);
     }
 
     private FixtureInventory entityToFixtureInventory(FixtureInventoryEntity fixtureInventoryEntity) {
