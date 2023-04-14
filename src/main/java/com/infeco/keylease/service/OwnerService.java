06838e45423fd6eac7e3f5819e96afc448c99569
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2023-04-14 10:15:55 +0200

    Updated Owner service to calculate the expectedRentAmount and the balance minus RentalFeesManagement

--- src/main/java/com/infeco/keylease/service/OwnerService.java
+++ src/main/java/com/infeco/keylease/service/OwnerService.java
@@ -1,14 +1,17 @@
 package com.infeco.keylease.service;
 
-import com.infeco.keylease.entity.AddressEntity;
-import com.infeco.keylease.entity.OwnerEntity;
+import com.infeco.keylease.entity.*;
 import com.infeco.keylease.exceptions.NotFoundEntity;
 import com.infeco.keylease.models.Address;
 import com.infeco.keylease.models.Owner;
 import com.infeco.keylease.repository.AddressRepository;
+import com.infeco.keylease.repository.LeaseContractRepository;
 import com.infeco.keylease.repository.OwnerRepository;
+import com.infeco.keylease.repository.RentalManagementFeesRepository;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -18,10 +21,14 @@ import java.util.stream.Collectors;
 public class OwnerService {
     private final OwnerRepository ownerRepository;
     private final AddressRepository addressRepository;
+    private final LeaseContractRepository leaseContractRepository;
+    private final RentalManagementFeesRepository rentalManagementFeesRepository;
 
-    public OwnerService(OwnerRepository ownerRepository, AddressRepository addressRepository) {
+    public OwnerService(OwnerRepository ownerRepository, AddressRepository addressRepository, LeaseContractRepository leaseContractRepository, RentalManagementFeesRepository rentalManagementFeesRepository) {
         this.ownerRepository = ownerRepository;
         this.addressRepository = addressRepository;
+        this.leaseContractRepository = leaseContractRepository;
+        this.rentalManagementFeesRepository = rentalManagementFeesRepository;
     }
 
     public List<Owner> getOwners() {
@@ -87,6 +94,46 @@ public class OwnerService {
     }
 
     private Owner entityToOwner(OwnerEntity ownerEntity) {
-        return new Owner(ownerEntity);
+        Owner owner = new Owner(ownerEntity);
+        setRentDataForOwner(owner, ownerEntity);
+        return owner;
+    }
+
+    /**
+     * Calcule le montant des loyers attendu et le solde actuel. Les frais de gestion sont soustraits
+     *
+     * @param owner
+     * @param ownerEntity
+     */
+    private void setRentDataForOwner(Owner owner, OwnerEntity ownerEntity) {
+        List<LeaseContractEntity> leaseContractEntityList = this.leaseContractRepository.getAllByOwnerId(ownerEntity.getId());
+        RentalManagementFeesEntity rentalManagementFeesEntity = this.rentalManagementFeesRepository.findAll().get(0);
+        List<PaymentEntity> paymentEntityList = new ArrayList<>();
+        BigDecimal expectedRentAmount = BigDecimal.ZERO;
+        Double feeRate = rentalManagementFeesEntity.getFeeRate();
+        for (LeaseContractEntity leaseContractEntity : leaseContractEntityList) {
+            expectedRentAmount = expectedRentAmount.add(expectedRentAmount(leaseContractEntity));
+            paymentEntityList.addAll(leaseContractEntity.getPayments());
+        }
+        BigDecimal expectedFees = expectedRentAmount.multiply(BigDecimal.valueOf(feeRate));
+        BigDecimal balance = BigDecimal.ZERO;
+        for (PaymentEntity paymentEntity : paymentEntityList) {
+            BigDecimal paidRent = paymentEntity.getPaidRent();
+            if (paymentEntity.getAmountPaidFromCafToOwner() != null) {
+                paidRent = paidRent.add(paymentEntity.getAmountPaidFromCafToOwner());
+            }
+            BigDecimal feesAmount = paidRent.multiply(BigDecimal.valueOf(feeRate));
+            balance = balance.add(paidRent.subtract(feesAmount));
+        }
+        owner.setExpectedRentAmount(expectedRentAmount.subtract(expectedFees));
+        owner.setBalance(balance);
+    }
+
+    private BigDecimal expectedRentAmount(LeaseContractEntity leaseContractEntity) {
+        BigDecimal expectedRentAmount = BigDecimal.ZERO;
+        if (leaseContractEntity.getPayments().size() > 0) {
+            expectedRentAmount = leaseContractEntity.getRentAmount().multiply(BigDecimal.valueOf(leaseContractEntity.getPayments().size()));
+        }
+        return expectedRentAmount;
     }
 }
